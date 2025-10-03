731d524df2dbfca0412fa0cee0e269e150c0f112
odutradev <joao.vitornl@gmail.com>
2025-10-03 10:18:11 -0300

    feat: Implementing absolute path not help command

--- lib/commands/help.ts
+++ lib/commands/help.ts
@@ -1,15 +1,14 @@
 import { Command } from 'commander';
+
 import { FileUtils } from '../utils/fileUtils.js';
+import { PathUtils } from '../utils/pathUtils.js';
 import { Logger } from '../utils/logger.js';
+
 import type { CommandOptions } from '../types/config.js';
 
 export class HelpCommand {
   public register(program: Command): void {
-    program
-      .command('help')
-      .description('Display help information')
-      .argument('[command]', 'Command to get help for')
-      .action((command?: string) => this.execute(program, command));
+    program.command('help').description('Display help information').argument('[command]', 'Command to get help for').action((command?: string) => this.execute(program, command));
   }
 
   private async execute(program: Command, command?: string): Promise<void> {
@@ -23,7 +22,7 @@ export class HelpCommand {
   }
 
   private displayBanner(): void {
-    const packageJson = FileUtils.readJson('./package.json') as any;
+    const packageJson = this.getPackageJson();
     const name = packageJson.name.replace('-', ' ').toUpperCase();
     Logger.figlet(name);
   }
@@ -33,19 +32,21 @@ export class HelpCommand {
       this.displayCommandHelp(program, command);
       return;
     }
-    
     program.help();
   }
 
   private displayCommandHelp(program: Command, commandName: string): void {
     const cmd = program.commands.find(c => c.name() === commandName);
-    
     if (cmd) {
       cmd.help();
       return;
     }
-    
     Logger.error(`Command '${commandName}' not found`);
     program.help();
+  }
+
+  private getPackageJson(): any {
+    const packagePath = PathUtils.getPackagePath(import.meta.url, 'package.json');
+    return FileUtils.readJson(packagePath);
   }
 }
